938d9ab147dbd811c157f90ffd8e62132c4200f7
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-25 15:54:17 -0400

    attempt2

--- server.js
+++ server.js
@@ -1,61 +1,24 @@
 const express = require("express");
-// const sequelize = require("sequelize");
-const routes = require("./routes");
-// const axios = require("axios");
-const app = express(); 
-var bodyParser = require('body-parser')
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-
-//For BodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// For Passport
-// app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
-
-// // Define middleware here
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-// // Serve up static assets (usually on heroku)
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-//routes
-// var authRoute = require('./routes/auth.js')(app, passport);
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+// Define API routes here
 
-const models = require("./models");
-
-//load passport strategies
-// require('./config/passport/passport.js')(passport, models.user);
-
-var syncOptions = { force: false };
-
-
-
-// Starting the server, syncing our models ------------------------------------/
-models.sequelize.sync(syncOptions).then(function() {
-  console.log("\nlet's plan a trip y'all!\n")
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
