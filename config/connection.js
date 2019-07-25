c5f8ac78815b14fa2b9e9f5ec64f05952f803735
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-25 16:40:40 -0400

    config

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+      host: "localhost",
+      port: 3301,
+      user: "root",
+      password: "tape88white",
+      database: "impromptu"
+});
+};
+
+// Make connection.
+connection.connect();
+  // function(err) {
+  //   if (err) {
+  //     console.error("error connecting: " + err.stack);
+  //     return;
+  //   }
+  //   console.log("connected as id " + connection.threadId);
+  // });
+  
+  // Export to ORM to use.
+  module.exports = connection;
