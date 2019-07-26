ede9b6ab136bc3fa89e17bd956f268ed8242d8fe
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-26 12:27:57 -0400

    test 4

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser")
-const routes = require("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,8 +13,6 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-app.use(routes)
 
 // Define API routes here
 
