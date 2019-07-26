aada7f3429b5a73f30ee42750e2e96f9bf362c4d
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-26 11:56:34 -0400

    excludereactroutefromroutefile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser")
+const routes = require ("./routes")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes)
 
 // Send every other request to the React app
 // Define any API routes before this runs
