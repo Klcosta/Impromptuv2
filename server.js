50aaea0fb1ec4b09c42cc13a228971d4fb718d7a
Costa <keith@Keiths-MacBook-Pro.local>
2019-07-26 12:54:23 -0400

    test5

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes")
 const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
+  app.use(routes)
   app.use(express.static("client/build"));
 }
 
